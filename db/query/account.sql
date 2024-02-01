38e38238ab1f10c256cd1ff013796f7d1f7dffbd
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-01-31 22:53:56 -0300

    added the 4 CRUD operations to the Accounts table. Will start creating the CRUD operations for other tables (entries and transfers) now

--- db/query/account.sql
+++ db/query/account.sql
@@ -15,3 +15,13 @@ WHERE id = $1 LIMIT 1;
 -- name: ListAccounts :many
 SELECT * FROM accounts
 ORDER BY id;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+  set balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
