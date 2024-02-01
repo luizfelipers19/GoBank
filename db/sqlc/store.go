00dc42492379880b81a0bbd659aeb3edc9babe6d
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-02-01 00:37:17 -0300

    started to implement the TransferTransaction method, and created their params body and result body that will serve as an output

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -20,7 +20,9 @@ func NewStore(db *sql.DB) *Store {
 	}
 }
 
-// executeTransaction function
+// -----------------------------------------------------------------------------------------------------------------------------------------------
+
+// executeTransaction function (generic function to be used as a helper to concrete implementations)
 func (store *Store) execTransaction(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -37,4 +39,34 @@ func (store *Store) execTransaction(ctx context.Context, fn func(*Queries) error
 	}
 
 	return tx.Commit()
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// contains the input that will be sent to the TransferTransaction operation
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// contains the Transfer result that will be the output of TransferTransaction operation
+type TransferTxResult struct {
+	TransferRecord Transfer `json:"transfer"`
+	FromAccount    Account  `json:"from_account"`
+	ToAccount      Account  `json:"to_account"`
+	FromEntry      Entry    `json:"from_entry"`
+	ToEntry        Entry    `json:"to_entry`
+}
+
+// TransferTransaction
+// performs all needed transactions in order to perform a whole transfer. Will Use the execTransactions func as a helper
+func (store *Store) TransferTransaction(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTransaction(ctx, func(q *Queries) error {
+		return nil
+	})
+
+	return result, err
 }
