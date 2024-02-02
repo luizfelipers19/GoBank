e952839e6091f8467994604f1049f109897ffcb9
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-02-02 16:24:03 -0300

    created a Start and errorResponse functions in the Server.go file, and created the CreateAccount inside the account.go that is our controller

--- api/account.go
+++ api/account.go
@@ -1,12 +1,35 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/luizfelipers19/gobank/db/sqlc"
+)
 
 type createAccountRequest struct {
-	Owner    string `json:"owner"`
-	Currency string `json:"currency"`
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required, oneof=USD EUR BRL"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req) {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
+	arg := db.CreateAccountParams{
+		Owner: req.Owner,
+		Currency: req.Currency,
+		Balance: 0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
 }
--- api/server.go
+++ api/server.go
@@ -17,9 +17,17 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("/accounts")
+	router.POST("/accounts", server.createAccount)
 
 	server.router = *router
 	return server
 
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
