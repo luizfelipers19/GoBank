2787c3ef7898b625f3e428f4d4bddf2a3bf25108
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-02-02 16:45:36 -0300

    created the Main.Go file in order to run the application. Note: imported the github.com/lib/pq in order to allow the connection with postgres. Without this, the application wont boot up

--- api/account.go
+++ api/account.go
@@ -14,15 +14,15 @@ type createAccountRequest struct {
 
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
-	if err := ctx.ShouldBindJSON(&req) {
+	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner:    req.Owner,
 		Currency: req.Currency,
-		Balance: 0,
+		Balance:  0,
 	}
 
 	account, err := server.store.CreateAccount(ctx, arg)
