66df4d03380956c3056e9276d6ffa39100a13723
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-02-04 23:07:04 -0300

    created a new route to deal with the GET request that retrieves all accounts. At this point, Ill be implementing pagination, and this GET process will be much similar to the GET that retrieves a single account by id

--- api/account.go
+++ api/account.go
@@ -59,3 +59,28 @@ func (server *Server) getAccountByID(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, account)
 }
+
+type listAccountsRequest struct {
+	PageID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req getAccountByIdRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccountByID(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+
+	}
+	ctx.JSON(http.StatusOK, account)
+}
--- api/server.go
+++ api/server.go
@@ -19,6 +19,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccountByID)
+	router.GET("/accounts", server.listAccounts)
 
 	server.router = *router
 	return server
