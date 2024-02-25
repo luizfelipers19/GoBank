5ef8b925370cf491b02afcc14422760c34b4ea12
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2024-02-25 14:46:23 -0300

    added a transfer handler inside our api folder that will be responsible of validating if the transfer is allowed

---
+++ api/transfer.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/luizfelipers19/gobank/db/sqlc"
+)
+
+type createTransferRequest struct {
+	FromAccountId int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR BRL"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req createTransferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !validateAccountCurrency(gin, req.FromAccountId, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountId,
+		ToAccountID:   req.ToAccountId,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTransaction(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, result)
+}
+
+func (server *Server) validateAccountCurrency(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := server.store.GetAccountByID(ctx, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: saved currency %s vs attempted currency %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
